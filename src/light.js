a492f806c54e34e953c039b0ca5c3b8fe45ad0a1
Justin Cordova <justcord10@gmail.com>
2023-10-17 15:40:42 -0700

    finished implementing light effects, got the fade in fade out effects working as intended in lights.js

--- src/light.js
+++ src/light.js
@@ -2,23 +2,23 @@ var Light = function(left, top, timer) {
   this.$node = $('<span class="light"></span>');
   this.timer = timer;
   this.setPosition(top, left);
-  this.fadeInLight()
+  this.fadeInLight();
 };
 
-  Light.prototype.fadeInLight = function() {
-    $(this).fadeIn(this.timer, this.fadeOutLight)
-  };
+Light.prototype.fadeInLight = function() {
+  this.$node.fadeIn(this.timer, () => this.fadeOutLight());
+};
 
-  Light.prototype.fadeOutLight = function() {
-    $(this).fadeOut(this.timer, this.fadeInLight)
-  };
+Light.prototype.fadeOutLight = function() {
+  this.$node.fadeOut(this.timer, () => this.fadeInLight());
+};
 
-  Light.prototype.setPosition = function(top, left) {
-    var styleSettings = {
-      top: top,
-      left: left
-    };
-    this.$node.css(styleSettings);
+Light.prototype.setPosition = function(top, left) {
+  var styleSettings = {
+    top: top,
+    left: left
   };
+  this.$node.css(styleSettings);
+};
 
 
